b09da13b833c808e61f7d1a4ab44a68b316da6bf
Subhrajyoti Naskar <subhrajyotinaskar98@gmail.com>
2020-09-01 21:07:31 +0530

    procfile

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.jsx'
-import * as serviceWorker from './serviceWorker';
+import App from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
